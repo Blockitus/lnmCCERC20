9ed5ffb809ca93c3dbbd01e1e1e09286f3483c17
pymachado <pyuris22@gmail.com>
2024-03-02 19:26:23 -0600

    feat: CCPTokenAndDataReceiver has allowed to mint tokens in the BCCLnM when CCIPTokenSender locks the amount to be minted

--- src/BCCLnM.sol
+++ src/BCCLnM.sol
@@ -6,9 +6,12 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BCCLnM is ERC20, OwnerIsCreator {
 
-    constructor() ERC20("Blockitus Cross Chain LnM", "BCCLnM") {}
+    constructor(address _airdrop) ERC20("Blockitus Cross Chain LnM", "BCCLnM") {
+        _mint(_airdrop, 100 ether);
+    }
 
     function mint(address beneficiary, uint256 amount) onlyOwner public {
         _mint(beneficiary, amount);
     }
+
 }
